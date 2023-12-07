a1262067258d809278765a21b296a755f58c2b80
Beloved1310 <adeoluwafisayomi@gmail.com>
2023-12-07 15:04:51 +0100

    fix/errors

--- dist/app.js
+++ dist/app.js
@@ -11,7 +11,7 @@ const cors_1 = __importDefault(require("cors"));
 (0, dotenv_1.config)();
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)({
-    origin: ["http://localhost:3000", "https://worknoon.vercel.app/", "https://worknoon-chisomije92.vercel.app/"],
+    origin: ["http://localhost:3000", "https://worknoon.vercel.app/", "https://worknoon-chisomije92.vercel.app/", "*"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     exposedHeaders: ["Set-Cookie"],
     credentials: true,
