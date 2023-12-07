0b36b69a33932a4345abf2cdab2b1f8275a8b7ad
Beloved1310 <adeoluwafisayomi@gmail.com>
2023-12-07 19:50:22 +0100

    fix/valid-user

--- middleware/validateUser.ts
+++ middleware/validateUser.ts
@@ -1,17 +1,21 @@
 import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 import { config } from '../config';
+import { userRepository } from '../repositories/user.repositories';
 
-export const validateUser = (req: any, res: Response, next: NextFunction) => {
-  const user = req.user?._id
-  if (!user) {
+export const validateUser = async (req: any, res: Response, next: NextFunction) => {
+  const email : string = req.user?.email
+
+  const userData : any= await userRepository.getOneUserData({email})
+  const {password, ...user} = userData.toObject()
+  if (!email) {
     return res
       .status(401)
       .send({ message: 'User Credentials not Valid' });
   }
 
   try {
-    res.status(200).json("User validated!");
+    res.status(200).send({message: "User validated!", user});
   } catch (ex) {
     res.status(500).send({ message: 'Invalid Credentials' });
     next(ex);
