75aaef60d4154655ba12f764cb079faa42faa79d
chisomije <chisomije92@gmail.com>
2023-12-10 14:55:04 +0100

    add helmet and resolve bug in cors

--- app.ts
+++ app.ts
@@ -1,42 +1,52 @@
-import express, { Application, Request, Response } from 'express'
-import user from './routes/user'
-import workspace from './routes/space'
-import { config } from 'dotenv'
-import cookieParser from 'cookie-parser';
-import cors from 'cors'
-import morgan from 'morgan';
-config()
+import express, { Application, Request, Response } from "express";
+import user from "./routes/user";
+import workspace from "./routes/space";
+import { config } from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import morgan from "morgan";
+import helmet from "helmet";
+config();
 
 declare global {
   namespace Express {
     interface Request {
-      user?: any
+      user?: any;
     }
   }
 }
 
-const app: Application = express()
+const app: Application = express();
 
-app.use(cors<Request>(
-  {
-    origin: ["http://localhost:3000", "https://worknoon.vercel.app/", "https://worknoon-chisomije92.vercel.app/", "*"],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-     exposedHeaders: ["Set-Cookie"],
+app.use(
+  cors<Request>({
+    origin: [
+      "http://localhost:3000",
+      "https://worknoon.vercel.app",
+      "https://worknoon-chisomije92.vercel.app",
+      "*",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    exposedHeaders: ["Set-Cookie"],
     credentials: true,
-  }
-))
+  })
+);
+
+app.use(
+  helmet({
+    crossOriginEmbedderPolicy: false,
+  })
+);
 app.use(cookieParser());
 app.use(morgan("common"));
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.get('/', (req: Request, res: Response) => {
-  return res.send('OK')
-})
+app.get("/", (req: Request, res: Response) => {
+  return res.send("OK");
+});
 
-app.use('/api/user', user)
-app.use('/api/space', workspace)
+app.use("/api/user", user);
+app.use("/api/space", workspace);
 
-
-
-export default app
+export default app;
--- dist/app.js
+++ dist/app.js
@@ -10,21 +10,30 @@ const dotenv_1 = require("dotenv");
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const cors_1 = __importDefault(require("cors"));
 const morgan_1 = __importDefault(require("morgan"));
+const helmet_1 = __importDefault(require("helmet"));
 (0, dotenv_1.config)();
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)({
-    origin: ["http://localhost:3000", "https://worknoon.vercel.app/", "https://worknoon-chisomije92.vercel.app/", "*"],
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    origin: [
+        "http://localhost:3000",
+        "https://worknoon.vercel.app",
+        "https://worknoon-chisomije92.vercel.app",
+        "*",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     exposedHeaders: ["Set-Cookie"],
     credentials: true,
+}));
+app.use((0, helmet_1.default)({
+    crossOriginEmbedderPolicy: false,
 }));
 app.use((0, cookie_parser_1.default)());
 app.use((0, morgan_1.default)("common"));
 app.use(express_1.default.urlencoded({ extended: true }));
 app.use(express_1.default.json());
-app.get('/', (req, res) => {
-    return res.send('OK');
+app.get("/", (req, res) => {
+    return res.send("OK");
 });
-app.use('/api/user', user_1.default);
-app.use('/api/space', space_1.default);
+app.use("/api/user", user_1.default);
+app.use("/api/space", space_1.default);
 exports.default = app;
